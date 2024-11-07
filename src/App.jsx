1498453f41c8cbcab35229304dbea5670f669ab8
Luis Rodellar <luisrnett@gmail.com>
2024-11-07 20:42:59 +0100

    add base portfolio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Container, SubTitle, Title } from "./App.styles";
+
+function App() {
+  return (
+    <Container>
+      <Title>Hola Mi nombre es Luis</Title>
+      <SubTitle>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
+        aperiam, magnam numquam autem voluptatem esse doloremque nam temporibus
+        quidem delectus ipsum assumenda nostrum voluptatum voluptas aspernatur.
+        Quae commodi distinctio similique!
+      </SubTitle>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/App.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Title = styled.h1`
+  font-size: 30px;
+  text-align: center;
+`;
+
+export const SubTitle = styled.p`
+  padding: 20px;
+  border: 1px solid red;
+`;
